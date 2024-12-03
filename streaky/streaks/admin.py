ea0047eadedf5d358bac3abb8738c85043b34792
anas-shakeel <srdranas456@gmail.com>
2024-12-04 00:09:25 +0500

    Add StreakAdmin class

--- streaky/streaks/admin.py
+++ streaky/streaks/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Streak
 
+
 # Register your models here.
-admin.site.register(Streak)
+class StreakAdmin(admin.ModelAdmin):
+    list_display = ("user", "title", "count", "date_added", "date_ended", "has_ended")
+
+
+admin.site.register(Streak, StreakAdmin)
