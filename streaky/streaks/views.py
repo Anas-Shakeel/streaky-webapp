fa3b3725c5937de557be8c1f651d2af40cedb4a7
anas-shakeel <srdranas456@gmail.com>
2024-12-07 00:27:33 +0500

    Add edit_streak view

--- streaky/streaks/views.py
+++ streaky/streaks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Streak
 
 
@@ -8,10 +8,17 @@ def home(request):
         return redirect("users:login")
 
     # Get Latest Streak
-    streak = Streak.objects.filter(has_ended=False)
-    if streak:
-        streak = streak[0]
-        streak_color = "orange"
+    streak = get_object_or_404(Streak, user=request.user)
+
+    # Decide color
+    if streak.count <= 50:
+        streak_color = "#40AFFF"
+    elif streak.count <= 100:
+        streak_color = "#40FF90"
+    elif streak.count <= 365:
+        streak_color = "#FFA640"
+    else:
+        streak_color = "#BF66FF"
 
     return render(
         request,
@@ -20,4 +27,22 @@ def home(request):
             "streak": streak,
             "streak_color": streak_color,
         },
+    )
+
+
+def edit_streak(request):
+    if not request.user.is_authenticated:
+        return redirect("users:login")
+
+    # Get User's streak
+    streak = get_object_or_404(Streak, user=request.user)
+
+    # Submitted Edit?
+    if request.method == "POST":
+        return redirect("streaks:home")
+
+    return render(
+        request,
+        "streaks/edit.html",
+        context={"streak": streak},
     )
