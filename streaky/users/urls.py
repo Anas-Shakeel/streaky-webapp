e33a16b52ff739435e1066b33efeeeba91d37f70
anas-shakeel <srdranas456@gmail.com>
2024-12-10 02:05:10 +0500

    Add change_username url

--- streaky/users/urls.py
+++ streaky/users/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("login/", views.login_user, name="login"),
     path("signup/", views.signup_user, name="signup"),
     path("logout/", views.logout_user, name="logout"),
+    path("change_username/", views.change_username, name="change_username"),
 ]
